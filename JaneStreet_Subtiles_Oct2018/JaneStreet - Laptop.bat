03dad00fb2fdc8604ce0d6bfe779a02cd0a20105
AnthonyDas <anthony.das@gmail.com>
2018-11-09 13:28:26 +0000

    Tidying .bat

--- JaneStreet_Subtiles_Oct2018/JaneStreet - Laptop.bat
+++ JaneStreet_Subtiles_Oct2018/JaneStreet - Laptop.bat
@@ -2,7 +2,7 @@ C:
 
 cd C:\Users\AnthonyDas\Documents\Visual Studio 2017\Projects\JaneStreet_Subtiles_Oct2018\JaneStreet_Subtiles_Oct2018
 
-g++ -Wall -Wconversion -std=c++11 -mtune=native -O3 -I"C:\Users\AnthonyDas\Documents\Visual Studio 2017\Projects\MathsHelper" -I"C:\Users\AnthonyDas\Documents\Visual Studio 2017\Projects\Common" Main.cpp -o Main.exe
+g++ -Wall -Wconversion -std=c++11 -mtune=native -O3 Main.cpp -o Main.exe
 
 
 pause
